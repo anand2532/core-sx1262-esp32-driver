97bdeaa8af6c4242ef64f6353262b7bb1ffcf6dd
anand <anandmohan.amp@gmail.com>
2025-10-28 13:37:25 +0530

    main/sx1262_hal.c

--- main/sx1262_hal.c
+++ main/sx1262_hal.c
@@ -53,13 +53,17 @@ esp_err_t sx1262_hal_gpio_init(const sx1262_hal_config_t *config)
     io_conf.intr_type = GPIO_INTR_DISABLE;
     ESP_ERROR_CHECK(gpio_config(&io_conf));
     
-    // Configure DIO1 pin as input
+    // Configure DIO1 pin as input with interrupt on rising edge
     io_conf.pin_bit_mask = (1ULL << config->dio1);
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
     ESP_ERROR_CHECK(gpio_config(&io_conf));
+    
+    // Install GPIO ISR service and add ISR handler for DIO1
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(config->dio1, NULL, NULL);
     
     // Configure DIO2 pin as input
     if (config->dio2 != GPIO_NUM_NC) {
